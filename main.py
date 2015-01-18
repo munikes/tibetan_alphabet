256d10808400b57f5d49f76df493a1e96d72cf9a
mUniKeS <munikes@members.fsf.org>
2015-01-18 02:19:37 +0100

    def new function generate question.

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ from unicodedata import lookup
 from Tkinter import *
 from pygame import mixer
 
-letras = {"ka":(lookup("tibetan letter ka"), "sound/ka.mp3"), 
-          "kha":(lookup("tibetan letter kha"), "sound/kha.mp3")}
+letras = {lookup("tibetan letter ka"):("ka", "sound/ka.mp3"), 
+          lookup("tibetan letter kha"):("k'a", "sound/kha.mp3"),
+          lookup("tibetan letter ga"):("k'a", "sound/kh_a.mp3"),
+          lookup("tibetan letter nga"):("nga", "sound/nga.mp3")}
+
+def generate_question(list_questions):
+    shuffle(list_questions)
+    right_answer = list_questions[0]
+    wrong_answers = [list_questions[1],list_questions[2],
+                     list_questions[3]]
+    return right_answer, wrong_answers
 
 def play_ogg():
     mixer.init()
@@ -22,10 +31,10 @@ label.pack()
 #            bitmap='snackPlay', command=play_ogg).place(x=115,y=501)
 option_1 = Button(ventana,width=50,height=50,fg='red', 
                   bitmap='snackRecord').place(x=5,y=501) 
-option_2=Button(ventana,width=50,height=50,fg='black', 
+option_2 = Button(ventana,width=50,height=50,fg='black', 
             bitmap='snackStop').place(x=60,y=501) 
-option_3=Button(ventana,width=50,height=50,fg='black', 
+option_3 = Button(ventana,width=50,height=50,fg='black', 
                 bitmap='snackPlay', command=play_ogg).place(x=115,y=501) 
-option_4=Button(ventana,width=5,height=3,fg='black', 
+option_4 = Button(ventana,width=5,height=3,fg='black', 
                 text='Save').place(x=170,y=501) 
 ventana.mainloop()
